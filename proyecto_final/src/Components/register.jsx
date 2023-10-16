d1abb192a644827d5e9e93e676757faab77c8a33
20Emi <iop.emilysanchez1@gmail.com>
2023-10-16 12:11:51 -0700

    sin terminar

--- proyecto_final/src/Components/register.jsx
+++ proyecto_final/src/Components/register.jsx
@@ -3,12 +3,14 @@ import {Envelope, Lock, Telephone, Person} from 'react-bootstrap-icons';
 
 export default function register() {
     return (
-      <>
-      <div className="'PANTALLA IZQUIERDA' w-2/4 h-full items-center justify-center flex">
-        <div className="'LOGO Y LOGIN' h-full w-3/5">
-          <div className="'LOGO DEL USUARIO' translate-y-1/2 rounded-full border-2 m-auto w-28 h-28 bg-white"></div>
-          <div className="'LOGIN' text-white py-3 h-custom p-4 mx-3.5 my-1.5 border-2 border-color-icons rounded-lg bg-cian-oscuro">
-            <form action="" className=" h-full pb-10 pt-12 px-1.5">
+    <>
+    <div className='w-2/5 flex items-center justify-center'>
+      {/* pantalla derecha */}
+      <div className="w-full h-screen bg-gray-200 flex items-center justify-center">
+        <div className="w-9/12 h-screen text-center mx-auto my-auto">
+        <div className="rounded-full border-2 mx-auto w-28 h-28 bg-white translate-y-1/2"></div>
+        <div className="text-white border-2 my-auto rounded-lg border-color-icons bg-cian-oscuro">
+            <form action="" className=" h-full pb-3.5 pt-20 px-6">
             <div className='relative my-2 text-xs'>
                 <Person className="absolute left-2 top-1/2 transform -translate-y-1/2 w-4 h-6 text-color-icons" />
                 <input type="Name" placeholder="Nombre del taller" className="rounded-md h-6 w-full bg-gris-claro text-black p-1 pl-8 placeholder-white" />
@@ -17,17 +19,16 @@ export default function register() {
                 <Telephone className="absolute left-2 top-1/2 transform -translate-y-1/2 w-4 h-6 text-color-icons" />
                 <input type="Number" placeholder="Número del taller" className="rounded-md h-6 w-full bg-gris-claro text-black p-1 pl-8 placeholder-white" />
             </div>
-            
               <div className='relative my-2 text-xs'>
-                <Envelope className="absolute left-2 top-1/2 transform -translate-y-1/2 w-4 h-6 text-color-icons" />
+              <Envelope className="absolute left-2 top-1/2 transform -translate-y-1/2 w-4 h-6 text-color-icons" />
                 <input type="Email" placeholder="Email" className="rounded-md h-6 w-full bg-gris-claro text-black p-1 pl-8 placeholder-white" />
               </div>
               <div className='relative my-1 text-xs'>
-                <Lock className="absolute left-2 top-3/4 transform -translate-y-3/4 w-4 h-6 text-color-icons" />
+                    <Lock className="absolute left-2 top-1/2 transform -translate-y-3/4 w-4 h-6 text-color-icons" />
                 <input type="Contraseña" placeholder="Contraseña" className="rounded-md h-6 w-full bg-gris-claro text-black p-1 pl-8 placeholder-white" />
               </div>
-              <div className='relative my-2 text-xs'>
-                <Lock className="absolute left-2 top-3/4 transform -translate-y-3/4 w-4 h-6 text-color-icons" />
+              <div className='relative my-1 text-xs'>
+                    <Lock className="absolute left-2 top-1/2 transform -translate-y-3/4 w-4 h-6 text-color-icons" />
                 <input type="Contraseña" placeholder="Repetir contraseña" className="rounded-md h-6  w-full bg-gris-claro text-black p-1 pl-8 placeholder-white" />
               </div>
               <div className="'BOTON LOGIN ">
@@ -43,6 +44,7 @@ export default function register() {
           </div>
         </div>
       </div>
+    </div>
       </>
     )
   }
